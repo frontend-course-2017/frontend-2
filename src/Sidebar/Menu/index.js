0436bfe917687c4bfe8c24669d3e7d349971f40c
frontend-course-2017 <frontend-course-2017@users.noreply.github.com>
2017-05-11 23:37:09 +0700

    Меню

---
+++ src/Sidebar/Menu/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './styles.css';
+
+export default () => (
+  <nav className="menu">
+    <div className="menu-item">
+      <div className="menu-item-link menu-item-link_active">
+        <a href="#">SPORTS</a>
+      </div>
+      <div className="menu-sub-items">
+        <div className="menu-sub-item menu-sub-item_active">
+          <span>SHOES</span>
+        </div>
+        <div className="menu-sub-item">
+          <a href="#">CLOTHING</a>
+        </div>
+        <div className="menu-sub-item">
+          <a href="#">ACCESORIES</a>
+        </div>
+      </div>
+    </div>
+    <div className="menu-item">
+      <div className="menu-item-link">
+        <a href="#">BRANDS</a>
+      </div>
+    </div>
+    <div className="menu-item">
+      <div className="menu-item-link">
+        <a href="#">MICOACH</a>
+      </div>
+    </div>
+  </nav>
+);
