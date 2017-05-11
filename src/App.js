a649463d4d0ff2a8d1d404582940e5a9c7c0cf85
frontend-course-2017 <frontend-course-2017@users.noreply.github.com>
2017-05-11 23:07:11 +0700

    Сайдбар

--- src/App.js
+++ src/App.js
@@ -1,57 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './styles.css';
+import Sidebar from './Sidebar';
 
 class App extends Component {
   render() {
     return (
       <div className="wrapper">
         <div className="content-two-column">
-          <aside className="sidebar">
-            <div className="logo">
-              <a href="#">
-                <img alt="Adidas" src="./images/logo.png" />
-              </a>
-            </div>
-            <div className="search-container">
-              <form className="search-form" name="search" action="" method="get">
-                <div className="search-submit-container">
-                  <input type="submit" value="Search" className="search-submit" />
-                </div>
-                <div className="search-input-container">
-                  <input title="Search" type="text" className="search-input" />
-                </div>
-              </form>
-            </div>
-            <nav className="menu">
-              <div className="menu-item">
-                <div className="menu-item-link menu-item-link_active">
-                  <a href="#">SPORTS</a>
-                </div>
-                <div className="menu-sub-items">
-                  <div className="menu-sub-item menu-sub-item_active">
-                    <span>SHOES</span>
-                  </div>
-                  <div className="menu-sub-item">
-                    <a href="#">CLOTHING</a>
-                  </div>
-                  <div className="menu-sub-item">
-                    <a href="#">ACCESORIES</a>
-                  </div>
-                </div>
-              </div>
-              <div className="menu-item">
-                <div className="menu-item-link">
-                  <a href="#">BRANDS</a>
-                </div>
-              </div>
-              <div className="menu-item">
-                <div className="menu-item-link">
-                  <a href="#">MICOACH</a>
-                </div>
-              </div>
-            </nav>
-          </aside>
+          <Sidebar />
           <section className="content">
             <div className="filter">
               <div className="filter-icon-wrapper">
