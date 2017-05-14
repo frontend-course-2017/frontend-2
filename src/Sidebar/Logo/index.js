ff1299a70f2c8fcfa4118085973ce3b494ef479f
frontend-course-2017 <frontend-course-2017@users.noreply.github.com>
2017-05-14 18:11:26 +0700

    fix: сделал ссылку на логотипе с помощью react-router

--- src/Sidebar/Logo/index.js
+++ src/Sidebar/Logo/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  NavLink
+} from 'react-router-dom';
 import './styles.css';
 
 export default () => (
   <div className="logo">
-    <a href="#">
-      <img alt="Adidas" src={require('./images/logo.png')} />
-    </a>
+    <Router>
+      <NavLink to="/">
+        <img alt="Adidas" src={require('./images/logo.png')} />
+      </NavLink>
+    </Router>
   </div>
 );
