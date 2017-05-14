9ddb2a9c3ddd9b27d495e6eceb6b275fe147ce5b
frontend-course-2017 <frontend-course-2017@users.noreply.github.com>
2017-05-15 00:05:47 +0700

    refactor: изменил условный вывод лэйбла SALE

--- src/Products/List/SaleBadge/index.jsx
+++ src/Products/List/SaleBadge/index.jsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function SaleBadge({ sale, className }) {
-  if (!sale) {
-    return (null);
-  }
-
-  return (
-    <span className={`sale-badge ${className}`}>
-      SALE
-    </span>
-  );
-}
+const SaleBadge = ({ sale, className }) => (
+  sale &&
+  <span className={`sale-badge ${className}`}>
+    SALE
+  </span>
+);
 
 SaleBadge.propTypes = {
   sale: PropTypes.bool.isRequired,
