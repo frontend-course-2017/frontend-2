e0884b443338d96adf748d0983c729cf5fec588d
frontend-course-2017 <frontend-course-2017@users.noreply.github.com>
2017-05-14 18:05:47 +0700

    fix: убрал console.log

--- src/Products/List/SaleBadge.js
+++ src/Products/List/SaleBadge.js
@@ -4,7 +4,6 @@ function SaleBadge({ sale, additionalClass }) {
   if (!sale) {
     return (null);
   }
-  console.log(additionalClass);
 
   return (
     <span className={`sale-badge ${additionalClass}`}>
