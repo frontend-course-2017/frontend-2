0d70c348a454cc2a15923d036a1a3e4240dfc965
frontend-course-2017 <frontend-course-2017@users.noreply.github.com>
2017-05-16 12:29:31 +0700

    refactor: change Card component alias in Cards

--- src/Products/List/Cards.jsx
+++ src/Products/List/Cards.jsx
@@ -1,26 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Row, Col } from 'react-flexbox-grid';
-import Item from './Card';
+import Card from './Card';
 
-const renderRowItems = (items = []) => (
+const renderRow = (items = []) => (
    items.map(item => (
-     <Col xs={12} sm={6} md={4} lg={3}>
-       <Item key={item.id} item={item} />
+     <Col xs={12} sm={6} md={4} lg={4}>
+       <Card key={item.id} item={item} />
      </Col>
    ))
 );
 
-function Cards({ items }) {
-  const products = renderRowItems(items);
-  return (
-    <div className="shoes-list">
-      <Row>
-        {products}
-      </Row>
-    </div>
-  );
-}
+const Cards = ({ items }) => (
+  <div className="shoes-list">
+    <Row>
+      {renderRow(items)}
+    </Row>
+  </div>
+);
 
 Cards.propTypes = {
   items: PropTypes.arrayOf(PropTypes.object).isRequired,
